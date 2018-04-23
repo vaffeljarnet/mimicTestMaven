81736968de4bf6b56de29296e58e435e532e32ec
larss_000 <larss_000@LAPTOP-1A0S11AO.lan>
2018-04-23 16:22:27 +0200

    Activated tests in TestDummySprint2.java

--- src/test/java/org/iths/mimicTests/sprint2/TestDummySprint2.java
+++ src/test/java/org/iths/mimicTests/sprint2/TestDummySprint2.java
@@ -7,7 +7,7 @@ public class TestDummySprint2 {
 	/**
 	 * Test for storing a request with response and answer directly
 	 */
-
+	@Test
 	public void test() {
 		DummySprint2 mock = new DummySprint2();
 		String response = mock.sendGetRequest("localhost:8080/1+1?storeRequest=2");
@@ -17,7 +17,7 @@ public class TestDummySprint2 {
 	/**
 	 * Test for storing a request with the wrong answer and then relearn it.
 	 */
-
+	@Test
 	public void test2() {
 		DummySprint2 mock = new DummySprint2();
 		String test = mock.sendGetRequest("localhost:8080/1+1?storeRequest=3");
@@ -28,7 +28,7 @@ public class TestDummySprint2 {
 	/**
 	 * Test for storing a request and then checking that we can ask that question.
 	 */
-
+	@Test
 	public void test3() {
 		DummySprint2 mock = new DummySprint2();
 		String test = mock.sendGetRequest("localhost:8080/1+1?storeRequest=2");
